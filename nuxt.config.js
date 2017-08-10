d06f7ef1fd6147b71921aec47147fdf1c3dab6d7
Ramiro <rhuayller@agetic.gob.bo>
2017-08-10 17:47:59 -0400

    se agrego vuetify base

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxtfvuetify',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLINT on save
+    */
+    vendor: ['vuetify'],
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  plugins: ['~plugins/vuetify'],
+  css: [
+    { src: '~assets/css/app.styl', lang: 'stylus' },
+    { src: '~assets/scss/main.scss', lang: 'scss' }
+  ],
+}
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div> test index page</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
