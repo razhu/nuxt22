333f9f4080485514099262ae5d42e677cb56b257
Ramiro <rhuayller@agetic.gob.bo>
2017-08-10 19:07:04 -0400

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div> test index page</div>
+  <div> 
+    <h1>random title</h1>
+    <photo-gallery></photo-gallery>
+    <stepper></stepper>
+  </div>
 </template>
 <script>
+import PhotoGallery from '~/components/Carrousel.vue'
+import Stepper from '~/components/Stepper.vue'
 export default {
-
+  components: {
+    PhotoGallery,
+    Stepper
+  }
 }
 </script>
 
